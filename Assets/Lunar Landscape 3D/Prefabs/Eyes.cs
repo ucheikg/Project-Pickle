6a8b606a09f6cb4638976f050e5d42ebdca11efb
UCHEOMA EMELONYE <UE315957@falmouth.ac.uk>
2024-10-17 16:44:49 +0100

    added in camera movement, changed envrioment

---
+++ Assets/Lunar Landscape 3D/Prefabs/Eyes.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eyes : MonoBehaviour
+{
+
+    public float sensx;
+    public float sensy;
+
+    public Transform orientation;
+    float xRotation;
+    float yRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    private void Update()
+    {
+        float mousex = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensx;
+        float mousey = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensy;
+
+        yRotation += mousex;
+        xRotation -= mousey;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+
+    }
+
+}
